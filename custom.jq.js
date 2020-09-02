2c3975b39826b1349d8a0e0b4fe7daa138cd14f2
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-09-02 10:24:46 +0200

    Update custom.jq.js

--- custom.jq.js
+++ custom.jq.js
@@ -31,7 +31,6 @@ if ($(window).width() < 767) {
     });
 }
 
-!function(r){function e(){for(var e,t,n=0;n&lt;l.length;n++)e=l[n],0&lt;=(t=e.getBoundingClientRect()).top&amp;&amp;0&lt;=t.left&amp;&amp;t.top&lt;=(r.innerHeight||document.documentElement.clientHeight)&amp;&amp;function(e,t){var n=new Image,r=e.getAttribute("data-src");n.onload=function(){e.parent?e.parent.replaceChild(n,e):e.src=r,t&amp;&amp;t()},n.src=r}(l[n],function(){l.splice(n,n)})}for(var l=new Array,t=function(e,t){if(document.querySelectorAll)t=document.querySelectorAll(e);else{var n=document,r=n.styleSheets[0]||n.createStyleSheet();r.addRule(e,"f:b");for(var l=n.all,c=0,o=[],i=l.length;c&lt;i;c++)l[c].currentStyle.f&amp;&amp;o.push(l[c]);r.removeRule(0),t=o}return t}("img.lazy"),n=0;n&lt;t.length;n++)l.push(t[n]);e(),function(e,t){r.addEventListener?this.addEventListener(e,t,!1):r.attachEvent?this.attachEvent("on"+e,t):this["on"+e]=t}("scroll",e)}(this);
 
 $(document).ready(function() {
     $('.thumbnail img').attr('data-src', function(i, src) {
