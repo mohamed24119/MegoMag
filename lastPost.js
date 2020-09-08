832d201ae7583c3ae3d8bf4b5d05f8515567091e
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-09-08 14:58:12 +0200

    Update lastPost.js

--- lastPost.js
+++ lastPost.js
@@ -2,6 +2,7 @@ function MigoMagLastPost(e) {
 for (var t = 0; t < numposts; t++) {
     var n = e.feed.entry[t];
     var postTitle = n.title.$t;
+    var postRandom = Math.floor(Math.random() * numposts.length);
     var postUrl;
     if (t == e.feed.entry.length) break;
     for (var o = 0; o < n.link.length; o++) {
