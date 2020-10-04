acdfc86728a530867e6e1ff2c36c6375298587a4
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-10-04 22:05:24 +0200

    Update lastPost.js

--- lastPost.js
+++ lastPost.js
@@ -4,125 +4,103 @@ for (var t = 0; t < numposts; t++) {
     var postTitle = n.title.$t;
     var postUrl;
     if (t == e.feed.entry.length) break;
-    for (var o = 0; o < n.link.length; o++) {
-        if (n.link[o].rel == "replies" && n.link[o].type == "text/html") {
-            var u = n.link[o].title;
-            var f = n.link[o].href;
+        for (var o = 0; o < n.link.length; o++) {
+            if (n.link[o].rel == "replies" && n.link[o].type == "text/html") {
+                var u = n.link[o].title;
+                var f = n.link[o].href;
+            }
+            if (n.link[o].rel == "alternate") {
+                postUrl = n.link[o].href;
+                break
+            }		
         }
-        if (n.link[o].rel == "alternate") {
-            postUrl = n.link[o].href;
-            break
+        var ellipsis = "...";
+        if (postTitle.length > 70) {
+            postTitle = postTitle.substring(0, 70) + " " + ellipsis;
         }
-    }
-    var ellipsis = "...";
-    if (postTitle.length > 70) {
-        postTitle = postTitle.substring(0, 70) + " " + ellipsis;
-    }
-    var postTime = n.published.$t;
-    var postTimeFormat = postTime.substring(0, 10);
-    var postAuthor = n.author[0].name.$t;
-    var postLabel = n.category[0].term;
-    var postID = n.id.$t.match(/\d+$/)[0];
-    var thumbnail;
-    try {
-        thumbnail = n.media$thumbnail.url
-    } catch (h) {
-        s = n.content.$t;
-        a = s.indexOf("<img");
-        b = s.indexOf('src="', a);
-        c = s.indexOf('"', b + 5);
-        d = s.substr(b + 5, c - b - 5);
-        if (a != -1 && b != -1 && c != -1 && d != "") {
-            thumbnail = d
-        } else thumbnail = "https://1.bp.blogspot.com/-igV9vkZfPhQ/X0KXOPzY91I/AAAAAAAADCI/fNMR6mXIHM04KHVkqvN9MsKbprYh2LSiACLcBGAsYHQ/s640/11.png"
-    }
+        var postTime = n.published.$t;
+        var postTimeFormat = postTime.substring(0, 10);
+        var postLabel = n.category[0].term;
+        var postID = n.id.$t.match(/\d+$/)[0];
+        var thumbnail;
+        try {
+            thumbnail = n.media$thumbnail.url
+        } catch (h) {
+            s = n.content.$t;
+            a = s.indexOf("<img");
+            b = s.indexOf('src="', a);
+            c = s.indexOf('"', b + 5);
+            d = s.substr(b + 5, c - b - 5);
+            if (a != -1 && b != -1 && c != -1 && d != "") {
+                thumbnail = d
+            } else thumbnail = "https://1.bp.blogspot.com/-igV9vkZfPhQ/X0KXOPzY91I/AAAAAAAADCI/fNMR6mXIHM04KHVkqvN9MsKbprYh2LSiACLcBGAsYHQ/s640/11.png"
+        }
 
-    if ("content" in n) {
-        var postSummary = n.content.$t;
-    } else {
-        var postSummary = "";
-    }
-    var re = /<\S[^>]*>/g;
-    postSummary = postSummary.replace(re, "");
-    if (postSummary.length > 170) {
-        postSummary = postSummary.substring(0, 170) + " " + ellipsis;
-    }
-    document.write('<article class="article-posts" id="post-id-'+ postID +'">');
-    document.write('<div class="box">');
-    if (showPostThumbnail == true) {
-        var printThumbnail = '<a class="thumbnail" href="' + postUrl + '"><img class="lazy" data-src="' + thumbnail + '" alt="' + postTitle + '"></a>';
-        document.write(printThumbnail);
-    }
-    
 
-    document.write('<div class="box-content">');
-    document.write('<header class="article-header"><h2 class="post-headding"><a class="link" href="' + postUrl + '">');
-    document.write(postTitle);
-     document.write('</a></h2></header>'); // end header
 
-    if (showPostSummary == true) {
-        if (postSummary != '') {
-            document.write('<p class="description">' + postSummary + '</p>');
-            //document.write(postSummary);
-            //document.write('</p>');
-        }
-    } // end description
+        document.write('<article class="article-posts" id="post-id-'+ postID +'">');
+            document.write('<div class="box">');
+                if (showPostThumbnail == true) {
+                    var printThumbnail = '<a class="thumbnail" href="' + postUrl + '"><img class="lazy" data-src="' + thumbnail + '" alt="' + postTitle + '"/></a>';
+                    document.write(printThumbnail);
+                }
 
-    if ((showTime == true) || (showPostLabel == true) || (showPostAuthor == true)) {
-        document.write('<footer class="article-footer">');
-        document.write('<div class="meta">');
-        if (showTime == true) {
-            
-            document.write('<time class="time" datetime="' + postTime + '" title="' + postTimeFormat + '">');
-            document.write(postTimeFormat);
-            document.write('</time>'); // end time
-        }
-        if (showPostLabel == true) {
-            document.write('<a class="label">');
-            document.write(postLabel);
-            document.write('</a>');
-        }
-        if (showPostAuthor == true) {
-            document.write('<a class="Author">');
-            document.write('<i class="fas fa-user"></i>' + ' ' + postAuthor);
-            document.write("</a>");
-        }
-        document.write('</div>'); // END meta
-        document.write('</footer>'); // end article-footer
-    }
+                document.write('<div class="box-content">');
 
-    document.write('</div>'); //end box-content
+                    document.write('<header class="article-header"><h2 class="post-headding"><a class="link" href="' + postUrl + '">');
+                        document.write(postTitle);
+                    document.write('</a></h2></header>'); // end header
+
+
+
+                if ((showTime == true) || (showPostLabel == true)) {
+                    document.write('<footer class="article-footer"><div class="meta">');
+                        if (showTime == true) {
+                            document.write('<time class="time" datetime="' + postTime + '" title="' + postTimeFormat + '">');
+                                document.write(postTimeFormat);
+                            document.write('</time>'); // end time
+                        }
+
+                        if (showPostLabel == true) {
+                            document.write('<a class="label">');
+                                document.write(postLabel);
+                            document.write('</a>');
+                        }
+
+                    document.write('</div></footer>'); // END meta
+              }
+
+        document.write('</div>'); //end box-content
     document.write('</div>'); //end box
-    document.write('</article>'); // end article
+document.write('</article>'); // end article
 }
 }
 
 
 var numposts = 5;
 var showPostThumbnail = true;
-var showPostSummary = false;
-var showPostAuthor = false;
 var showTime = true;
 var showPostLabel = true;
 
 
 function getLastPosts(url, name, style) {
 document.write('<div class="featured ' + style + '">');
-document.write('<div class="cat-title">');
-document.write('<a href="' + url + '/search">' + name + '</a>');
-document.write('</div>');
-document.write('<div class="articles">');
-document.write('<script src="' + url + '/feeds/posts/default/?orderby=published&alt=json-in-script&callback=MigoMagLastPost"></script>');
-document.write('</div>');
+    document.write('<div class="cat-title">');
+        document.write('<a href="' + url + '/search">' + name + '</a>');
+    document.write('</div>');
+        document.write('<div class="articles">');
+            document.write('<script src="' + url + '/feeds/posts/default/?orderby=published&alt=json-in-script&callback=MigoMagLastPost"></script>');
+        document.write('</div>');
 document.write('</div>');
 }
+
 function getLastPostsLabel(url, label, style) {
 document.write('<div class="featured ' + style + '">');
-document.write('<div class="cat-title">');
-document.write('<a href="' + url + '/search/label/' + label + ' ">' + label + '</a>');
-document.write('</div>');
-document.write('<div class="articles">');
-document.write('<script src="' + url + '/feeds/posts/default/-/' + label + '?orderby=published&alt=json-in-script&callback=MigoMagLastPost"></script>');
-document.write('</div>');
+    document.write('<div class="cat-title">');
+        document.write('<a href="' + url + '/search/label/' + label + ' ">' + label + '</a>');
+    document.write('</div>');
+    document.write('<div class="articles">');
+        document.write('<script src="' + url + '/feeds/posts/default/-/' + label + '?orderby=published&alt=json-in-script&callback=MigoMagLastPost"/></script>');
+    document.write('</div>');
 document.write('</div>');
 }
