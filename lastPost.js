747897f6a33c9ca7fd9a7cdfd2a629731093ed0f
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-10-15 20:06:53 +0200

    Update lastPost.js

--- lastPost.js
+++ lastPost.js
@@ -108,10 +108,8 @@ function getLastPostsSlide(url,name,style,id){
     document.write('<div class="cat-title">');
     document.write('<a href="' + url + '/search">' + name + '</a>');
     document.write('</div>');
-    document.write('<div class="articles">');
+    document.write('<div class="articles owl-carousel owl-theme" id="'+ id +'">');
     document.write('<script src="' + url + '/feeds/posts/default/?orderby=published&alt=json-in-script&callback=MigoMagLastPost"></script>');
     document.write('</div>');
     document.write('</div>');
 }
-
-
