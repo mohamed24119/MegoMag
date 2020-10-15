627d458df1d20a5f0f6d6c0e375d2b5dc62fce5e
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-10-15 23:13:24 +0200

    Update lastPost.js

--- lastPost.js
+++ lastPost.js
@@ -88,11 +88,10 @@ function getLastPostsLabel(url, label, style) {
     document.write('<div class="featured ' + style + '"><div class="cat-title"><a href="' + url + '/search/label/' + label + ' ">' + label + '</a></div><div class="articles"><script src="' + url + '/feeds/posts/default/-/' + label + '?orderby=published&alt=json-in-script&callback=MigoMagLastPost"/></script></div></div>');
 }
 
-if(blogId === 5696156054658424367){
+
     function getLastPostsSlide(url, name, style, slidId){
         document.write('<div class="featured ' + style + '"><div class="cat-title"><a href="' + url + '/search">' + name + '</a></div><div class="articles owl-carousel" id="' + slidId + '"><script src="' + url + '/feeds/posts/default/?orderby=published&alt=json-in-script&callback=MigoMagLastPost"></script></div></div>');
     }
 
-} else{
-alert('يجب تفعيل القالب');
-}
+alert(blogId);
+
