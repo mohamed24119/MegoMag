f67fb2bc24bf795c3786f94d1ae18373e6e271ad
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-11-14 16:20:15 +0200

    Update lastPost.js

--- lastPost.js
+++ lastPost.js
@@ -38,8 +38,14 @@ try {
 document.write('<article class="article-posts" id="post-id-' + postID + '">');
 document.write('<div class="box">');
 if (showPostThumbnail == true) {
+	if(showLozad == true){
 		var printThumbnail = '<a class="thumbnail" href="' + postUrl + '"><img class="lozad" loading="lazy" data-src="' + thumbnail + '" alt="' + postTitle + '"/></a>';
 		document.write(printThumbnail);
+	} else {
+		var printThumbnail = '<a class="thumbnail" href="' + postUrl + '"><img class="lozad" loading="lazy" src="' + thumbnail + '" alt="' + postTitle + '"/></a>';
+		document.write(printThumbnail);
+	}
+	
 }
 
 document.write('<div class="box-content">');
@@ -61,6 +67,7 @@ document.write('</div></div></article>');
 
 var showPostThumbnail = true;
 var showTime = true;
+var showLozad = true;
 
 
 function getLastPosts(url, name, style,maxResults) {
