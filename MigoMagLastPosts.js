1122c649a20b87f4b2faf89788db5184f43109f7
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-11-17 14:41:17 +0200

    Update MigoMagLastPosts.js

--- MigoMagLastPosts.js
+++ MigoMagLastPosts.js
@@ -1,56 +1,54 @@
 function lastPosts(json) {
-	for (i = 0; i < json.feed.entry.length; i++){
-		var jfei = json.feed.entry[i];
-		for (j = 0; j < jfei.link.length; j++) {
-			if (jfei.link[j].rel == 'alternate') {
-				break;
-			}
+for (i = 0; i < json.feed.entry.length; i++){
+	var jfei = json.feed.entry[i];
+	for (j = 0; j < jfei.link.length; j++) {
+		if (jfei.link[j].rel == 'alternate') {
+			break;
 		}
-		var postUrl = jfei.link[j].href;
-		var postTitle = jfei.title.$t;
-        var thumbnail;
-        try {
-            thumbnail = jfei.media$thumbnail.url;
-        } catch (h) {
-            s = jfei.content.$t;
-            a = s.indexOf("<img");
-            b = s.indexOf('src="', a);
-            c = s.indexOf('"', b + 5);
-            d = s.substr(b + 5, c - b - 5);
-            if (a != -1 && b != -1 && c != -1 && d != "") {
-                thumbnail = d;
-            } else{
-            thumbnail = "https://1.bp.blogspot.com/-igV9vkZfPhQ/X0KXOPzY91I/AAAAAAAADCI/fNMR6mXIHM04KHVkqvN9MsKbprYh2LSiACLcBGAsYHQ/s640/11.png";
-            }
-        }
-
-        var fixThumbnail = thumbnail.replace("s72-c", "s1600");
-		var printElements = '<article class="card article-posts"><a class="thumbnail" href="#"><img class="card-img-top" src="'+ fixThumbnail +'" alt=""></a><div class="card-body"><h2><a href="#"></a></h2></div></article>';
-        document.write('<div class="swiper-slide">'+ printElements +'</div>');
 	}
+	var postUrl = jfei.link[j].href;
+	var postTitle = jfei.title.$t;
+var thumbnail;
+try {
+    thumbnail = jfei.media$thumbnail.url;
+} catch (h) {
+    s = jfei.content.$t;
+    a = s.indexOf("<img");
+    b = s.indexOf('src="', a);
+    c = s.indexOf('"', b + 5);
+    d = s.substr(b + 5, c - b - 5);
+    if (a != -1 && b != -1 && c != -1 && d != "") {
+	thumbnail = d;
+    } else{
+    thumbnail = "https://1.bp.blogspot.com/-igV9vkZfPhQ/X0KXOPzY91I/AAAAAAAADCI/fNMR6mXIHM04KHVkqvN9MsKbprYh2LSiACLcBGAsYHQ/s640/11.png";
+    }
+}
+
+var fixThumbnail = thumbnail.replace("s72-c", "s1600");
+	var printElements = '<article class="card article-posts"><a class="thumbnail" href="#"><img class="card-img-top" src="'+ fixThumbnail +'" alt=""></a><div class="card-body"><h2><a href="#"></a></h2></div></article>';
+document.write('<div class="swiper-slide">'+ printElements +'</div>');
+}
 
 }
 
 function getLastPosts(url,max){
-    if(url == null || url == ''){
-        var url = 'https://www.mohamed24119.com';
-    }
-
-    if(max == null || max == ''){
-        var max = 5;
-    }
-	document.write('<div class="featured grid"><div class="articles"><script src="'+ url +'/feeds/posts/default?alt=json-in-script&max-results='+ max +'&callback=lastPosts"></script></div></div>');
+if(url == null || url == ''){
+var url = 'https://www.mohamed24119.com';
 }
 
-function getLastPostsSlide(url,max,slideId){
-    if(url == null || url == ''){
-        var url = 'https://www.mohamed24119.com';
-    }
+if(max == null || max == ''){
+var max = 5;
+}
+document.write('<div class="featured grid"><div class="articles"><script src="'+ url +'/feeds/posts/default?alt=json-in-script&max-results='+ max +'&callback=lastPosts"></script></div></div>');
+}
 
-    if(max == null || max == ''){
-        var max = 5;
-    }
-    if(slideId == null || slideId == ''){
-    }
-	document.write('<div class="featured grid"><div class="swiper-container"><div class="articles swiper-wrapper"><script src="'+ url +'/feeds/posts/default?alt=json-in-script&max-results='+ max +'&callback=lastPosts"></script></div><div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div></div></div>');
+function getLastPostsSlide(url,max){
+if(url == null || url == ''){
+var url = 'https://www.mohamed24119.com';
+}
+
+if(max == null || max == ''){
+	var max = 5;
+}
+document.write('<div class="featured grid"><div class="swiper-container"><div class="articles swiper-wrapper"><script src="'+ url +'/feeds/posts/default?alt=json-in-script&max-results='+ max +'&callback=lastPosts"></script></div><div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div></div></div>');
 }
