eb1e7a4e6922ce8bc921d7d50e54e763f0687cd9
Mohamed Refaat <58652163+mohamed24119@users.noreply.github.com>
2020-11-17 14:16:41 +0200

    Update MigoMagLastPosts.js

--- MigoMagLastPosts.js
+++ MigoMagLastPosts.js
@@ -39,5 +39,7 @@ function getLastPosts(url,max){
     if(max == null || max == ''){
         var max = 5;
     }
-  document.write('<script src="'+ url +'/feeds/posts/default?alt=json-in-script&max-results='+ max +'&callback=lastPosts"></script>');
+	document.write('<div class="featured"><div class="swiper-container swiper-1"><div class="articles swiper-wrapper"><script src="'+ url +'/feeds/posts/default?alt=json-in-script&max-results='+ max +'&callback=lastPosts"></script></div><div class="swiper-pagination"></div><div class="swiper-button-prev"></div><div class="swiper-button-next"></div></div></div>');
+
+  // document.write('<script src="'+ url +'/feeds/posts/default?alt=json-in-script&max-results='+ max +'&callback=lastPosts"></script>');
 }
